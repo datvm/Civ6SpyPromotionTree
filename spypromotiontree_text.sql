69803c01629031b352bc3db18cb2aa584773e396
Luke Vo <datvm@outlook.com>
2019-05-22 08:28:36 +0700

    Initialize version

---
+++ spypromotiontree_text.sql
@@ -0,0 +1,13 @@
+INSERT OR REPLACE INTO EnglishText("Tag", "Text") VALUES
+	('LOC_PROMOTION_SPY_COUNTERINTELLIGENCE_NAME', 'Counter Intelligence'),
+	('LOC_PROMOTION_SPY_MASTERMISCHIEF_NAME', 'Master Mischief'),
+	('LOC_PROMOTION_SPY_SECRETIVELEADER_NAME', 'Secretive Leader'),
+	('LOC_PROMOTION_SPY_QUICKDRAW_NAME', 'Quick Draw'),
+	('LOC_PROMOTION_SPY_ASSASSIN_NAME', 'Assassin'),
+	('LOC_PROMOTION_SPY_MANINSUIT_NAME', 'Man in Suit'),
+	('LOC_PROMOTION_SPY_COUNTERINTELLIGENCE_DESCRIPTION', 'Counterspying is at +2 level more experienced and all city districts are defended (+1 more at districts within 1 hex) and enemies Spies in your land operate at -1 level.'),
+	('LOC_PROMOTION_SPY_MASTERMISCHIEF_DESCRIPTION', 'Sabotage Production, Fabricate Scandal and Breach Dam at +2 levels.'),
+	('LOC_PROMOTION_SPY_SECRETIVELEADER_DESCRIPTION', 'Recruit Partisans and Foment Unrest at +2 level. If this Spy is in home territory, all your Spies operate at +1 level.'),
+	('LOC_PROMOTION_SPY_QUICKDRAW_DESCRIPTION', 'Siphon Funds at +2 level and Escape at +4 level. Time to complete missions reduced by 25%.'),
+	('LOC_PROMOTION_SPY_ASSASSIN_DESCRIPTION', 'Takes no time to establish in enemy cities. Neutralize Governor and Steal Great Works at +2 levels.'),
+	('LOC_PROMOTION_SPY_MANINSUIT_DESCRIPTION', 'Steal Technology and Disrupt Rocketry at +2 level.');
