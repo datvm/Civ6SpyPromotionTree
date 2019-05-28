cffe33b0507bb163d989f3ca08ccfa258e1fa637
Luke Vo <datvm@outlook.com>
2019-05-28 16:19:45 +0700

    Fixed problem for R&F and vanilla game. Fixed text problem when replacing text.

--- spypromotiontree_text.sql
+++ spypromotiontree_text.sql
@@ -12,5 +12,8 @@ INSERT OR REPLACE INTO EnglishText("Tag", "Text") VALUES
 	('LOC_PROMOTION_SPY_MASTERTHIEF_DESCRIPTION', 'Siphon Funds and Steal Great Works at +2 levels.'),
 	('LOC_PROMOTION_SPY_DIRTYPOLITICS_DESCRIPTION', 'Fabricate Scandals, Foment Unrest and Recruit Partisans at +2 levels.'),
 	('LOC_PROMOTION_SPY_ASSASSIN_DESCRIPTION', 'Takes no time to establish in enemy cities. Neutralize Governor at +2 levels. Escape at +4 level if caught.'),
-	('LOC_PROMOTION_SPY_MANINSUIT_DESCRIPTION', 'Steal Technology and Disrupt Rocketry at +2 level. Time to complete all missions reduced by 25%.'),
-	('LOC_POLICY_FUTURE_COUNTER_SCIENCE_DESCRIPTION', 'You Spies start with 2 promotions (3 for Catherine).');
+	('LOC_PROMOTION_SPY_MANINSUIT_DESCRIPTION', 'Steal Technology and Disrupt Rocketry at +2 level. Time to complete all missions reduced by 25%.');
+
+UPDATE LocalizedText
+	SET Text = 'You Spies start with 2 promotions (3 for Catherine).'
+	WHERE Tag = 'LOC_POLICY_FUTURE_COUNTER_SCIENCE_DESCRIPTION' AND Language = 'en_US';
